a1da520aa9b244ab4e1df151a4228318bdd68d67
Lia Forgione <liaforg@gmail.com>
2017-06-08 14:27:46 -0400

    instance variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
 
 def welcome
+  @header = "Welcome to the welcome page"
 end
 
 def about
+  @header = "welcome to the about page"
 end
 
 def contest
+  @header = "welcome to the contest page"
 end
 
 end
